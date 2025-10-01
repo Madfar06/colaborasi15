bccac9d70e3cf2fb83410436c8cfab71403b014d
sixzenith-marvel <marvel@sixzenith.com>
2025-10-01 19:20:12 +0700

    Marvel ngajak login pubg

--- tugasKolaborasi15.java
+++ tugasKolaborasi15.java
@@ -2,5 +2,6 @@ public class tugasKolaborasi15 {
     public static void main(String[] args) {
         System.out.println("gtw bro");
         System.out.println("aku jg bingung masbro");
+        System.out.println("daripada galau mending login dah");
     }
 }
