ea26d26fb42bac55d6134daf13c050c1847e3e65
sixzenith-marvel <marvel@sixzenith.com>
2025-10-01 19:16:04 +0700

    Marvel mengedit tugasKolaborasi15

--- tugasKolaborasi15.java
+++ tugasKolaborasi15.java
@@ -1,5 +1,6 @@
 public class tugasKolaborasi15 {
     public static void main(String[] args) {
         System.out.println("gtw bro");
+        System.out.println("aku jg bingung masbro");
     }
 }
