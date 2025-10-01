9d9dd9295d166370a3b2a0513a8fbd70dd13fc38
Madfar06 <gfrnrl19@gmail.com>
2025-10-01 23:04:48 +0700

    berikan saya pengendali yang lebih bagus

--- tugasKolaborasi15.java
+++ tugasKolaborasi15.java
@@ -1,7 +1,11 @@
 public class tugasKolaborasi15 {
     public static void main(String[] args) {
+       int i = 200;
+
         System.out.println("gtw bro");
         System.out.println("aku jg bingung masbro");
         System.out.println("daripada galau mending login dah");
+        System.out.println("beri aku " + i );
+        
     }
 }
