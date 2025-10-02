7376d73e51b8873d393640c6ab4ace301617a547
Madfar06 <gfrnrl19@gmail.com>
2025-10-02 12:59:59 +0700

    Update tugasKolaborasi15.java

--- tugasKolaborasi15.java
+++ tugasKolaborasi15.java
@@ -1,11 +1,10 @@
 public class tugasKolaborasi15 {
     public static void main(String[] args) {
-       int i = 200;
+       int i = 1000;
 
         System.out.println("gtw bro");
         System.out.println("aku jg bingung masbro");
         System.out.println("daripada galau mending login dah");
-        System.out.println("beri aku " + i );
-        
+        System.out.println("hari ini malas" + i);
     }
 }
